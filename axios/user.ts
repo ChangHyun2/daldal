9ac9f1e7360c0590435c4155b91bc52955c98543
Changhyun Jun <changhyunjun@Changhyunui-MacBookPro.local>
2023-04-01 18:15:28 +0900

    fix

--- axios/user.ts
+++ axios/user.ts
@@ -1,14 +1,14 @@
-import { User } from "@/data/backend/member";
+import { Member } from "@/data/backend/member";
 import axios from "axios";
 
 // GET
-export const getUser = (id: Pick<User, "id">) => axios.get(`/user/${id}`);
+export const getUser = (id: Pick<Member, "id">) => axios.get(`/user/${id}`);
 
 // PATCH
-export type PatchUserRequest = Pick<User, "id"> & Partial<User>;
-export type PatchUserResponse = User;
+export type PatchUserRequest = Pick<Member, "id"> & Partial<Member>;
+export type PatchUserResponse = Member;
 
 export const patchUser = (
-  id: Pick<User, "id">,
+  id: Pick<Member, "id">,
   patchUserRequest: PatchUserRequest
 ) => axios.patch<PatchUserResponse>(`/user/${id}`, patchUserRequest);
